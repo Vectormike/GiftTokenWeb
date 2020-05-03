859f74d6650a7a02e8e6913782cff1c6d29d2e08
Redeemer40 <victorjonah199@gmail.com>
2020-05-03 16:28:44 +0100

    ...

--- imports/frontend/Team.jsx
+++ imports/frontend/Team.jsx
@@ -14,7 +14,7 @@ export class Team extends Component {
           <div id="row">
             {this.props.data
               ? this.props.data.map((d) => (
-                  <div className="col-md-3 col-sm-6 team">
+                  <div className="col-md-4 col-sm-6 team">
                     <div className="thumbnail">
                       {" "}
                       <img src={d.img} alt="..." className="team-img" />
--- imports/frontend/services.jsx
+++ imports/frontend/services.jsx
@@ -7,10 +7,10 @@ export class Services extends Component {
         <div className="container">
           <div className="section-title">
             <h2>Our Services</h2>
-            <p>
+            {/* <p>
               Lorem ipsum dolor sit amet, consectetur adipiscing elit duis sed
               dapibus leonec.
-            </p>
+            </p> */}
           </div>
           <div className="row">
             {this.props.data
