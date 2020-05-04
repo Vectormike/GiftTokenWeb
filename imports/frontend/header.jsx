dd504b1f515445e0d94df4642f85f21a8a116b2c
Redeemer40 <victorjonah199@gmail.com>
2020-05-04 18:54:15 +0100

    ...

--- imports/frontend/header.jsx
+++ imports/frontend/header.jsx
@@ -1,32 +1,35 @@
 import React, { Component } from "react";
+import Slide from "react-reveal/Slide";
 
 export class Header extends Component {
   render() {
     return (
       <header id="header">
-        <div className="intro">
-          <div className="overlay">
-            <div className="container">
-              <div className="row">
-                <div className="col-md-8 col-md-offset-2 intro-text">
-                  <h1>
-                    {this.props.data ? this.props.data.title : "Loading"}
-                    <span></span>
-                  </h1>
-                  <p>
-                    {this.props.data ? this.props.data.paragraph : "Loading"}
-                  </p>
-                  <a
-                    href="#about"
-                    className="btn btn-custom btn-lg page-scroll"
-                  >
-                    Learn More
-                  </a>{" "}
+        <Slide top>
+          <div className="intro">
+            <div className="overlay">
+              <div className="container">
+                <div className="row">
+                  <div className="col-md-8 col-md-offset-2 intro-text">
+                    <h1>
+                      {this.props.data ? this.props.data.title : "Loading"}
+                      <span></span>
+                    </h1>
+                    <p>
+                      {this.props.data ? this.props.data.paragraph : "Loading"}
+                    </p>
+                    <a
+                      href="#about"
+                      className="btn btn-custom btn-lg page-scroll"
+                    >
+                      Learn More
+                    </a>{" "}
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </Slide>
       </header>
     );
   }
