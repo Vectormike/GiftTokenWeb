ca2af3bac78e9df4d97794a46914a2b32f11af9e
Redeemer40 <victorjonah199@gmail.com>
2020-05-04 18:54:21 +0100

    ...

--- imports/frontend/Team.jsx
+++ imports/frontend/Team.jsx
@@ -1,33 +1,36 @@
 import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
 
 export class Team extends Component {
   render() {
     return (
       <div id="team" className="text-center">
-        <div className="container">
-          <div className="col-md-8 col-md-offset-2 section-title">
-            <h2>Meet the Team</h2>
-            <p>
-              Meet our hard-working team making your experience a top-notch.
-            </p>
-          </div>
-          <div id="row">
-            {this.props.data
-              ? this.props.data.map((d) => (
-                  <div className="col-md-4 col-sm-6 team">
-                    <div className="thumbnail">
-                      {" "}
-                      <img src={d.img} alt="..." className="team-img" />
-                      <div className="caption">
-                        <h4>{d.name}</h4>
-                        <p>{d.job}</p>
+        <Fade bottom>
+          <div className="container">
+            <div className="col-md-8 col-md-offset-2 section-title">
+              <h2>Meet the Team</h2>
+              <p>
+                Meet our hard-working team making your experience a top-notch.
+              </p>
+            </div>
+            <div id="row">
+              {this.props.data
+                ? this.props.data.map((d) => (
+                    <div className="col-md-4 col-sm-6 team">
+                      <div className="thumbnail">
+                        {" "}
+                        <img src={d.img} alt="..." className="team-img" />
+                        <div className="caption">
+                          <h4>{d.name}</h4>
+                          <p>{d.job}</p>
+                        </div>
                       </div>
                     </div>
-                  </div>
-                ))
-              : "loading"}
+                  ))
+                : "loading"}
+            </div>
           </div>
-        </div>
+        </Fade>
       </div>
     );
   }
--- imports/frontend/gallery.jsx
+++ imports/frontend/gallery.jsx
@@ -1,81 +1,84 @@
 import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
 
 export class Gallery extends Component {
   render() {
     return (
       <div id="portfolio" className="text-center">
-        <div className="container">
-          <div className="section-title">
-            <h2>Gallery</h2>
-          </div>
-          <div className="row">
-            <div className="portfolio-items">
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/01-large.jpg"
-                      title=""
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>We are buying fast</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/01-small.jpg"
-                        className="img-responsive"
-                        alt=""
-                      />{" "}
-                    </a>{" "}
+        <Fade bottom>
+          <div className="container">
+            <div className="section-title">
+              <h2>Gallery</h2>
+            </div>
+            <div className="row">
+              <div className="portfolio-items">
+                <div className="col-sm-6 col-md-4 col-lg-4">
+                  <div className="portfolio-item">
+                    <div className="hover-bg">
+                      {" "}
+                      <a
+                        href="img/portfolio/01-large.jpg"
+                        title=""
+                        data-lightbox-gallery="gallery1"
+                      >
+                        <div className="hover-text">
+                          <h4>We are buying fast</h4>
+                        </div>
+                        <img
+                          src="img/portfolio/01-small.jpg"
+                          className="img-responsive"
+                          alt=""
+                        />{" "}
+                      </a>{" "}
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/05-large.jpg"
-                      title=""
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>Just the Truth</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/05-small.jpeg"
-                        className="img-responsive"
-                        alt=""
-                      />{" "}
-                    </a>{" "}
+                <div className="col-sm-6 col-md-4 col-lg-4">
+                  <div className="portfolio-item">
+                    <div className="hover-bg">
+                      {" "}
+                      <a
+                        href="img/portfolio/05-large.jpg"
+                        title=""
+                        data-lightbox-gallery="gallery1"
+                      >
+                        <div className="hover-text">
+                          <h4>Just the Truth</h4>
+                        </div>
+                        <img
+                          src="img/portfolio/05-small.jpeg"
+                          className="img-responsive"
+                          alt=""
+                        />{" "}
+                      </a>{" "}
+                    </div>
                   </div>
                 </div>
-              </div>
-              <div className="col-sm-6 col-md-4 col-lg-4">
-                <div className="portfolio-item">
-                  <div className="hover-bg">
-                    {" "}
-                    <a
-                      href="img/portfolio/07-large.jpg"
-                      title=""
-                      data-lightbox-gallery="gallery1"
-                    >
-                      <div className="hover-text">
-                        <h4>ROI</h4>
-                      </div>
-                      <img
-                        src="img/portfolio/07-small.jpeg"
-                        className="img-responsive"
-                        alt=""
-                      />{" "}
-                    </a>{" "}
+                <div className="col-sm-6 col-md-4 col-lg-4">
+                  <div className="portfolio-item">
+                    <div className="hover-bg">
+                      {" "}
+                      <a
+                        href="img/portfolio/07-large.jpg"
+                        title=""
+                        data-lightbox-gallery="gallery1"
+                      >
+                        <div className="hover-text">
+                          <h4>ROI</h4>
+                        </div>
+                        <img
+                          src="img/portfolio/07-small.jpeg"
+                          className="img-responsive"
+                          alt=""
+                        />{" "}
+                      </a>{" "}
+                    </div>
                   </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
+        </Fade>
       </div>
     );
   }
