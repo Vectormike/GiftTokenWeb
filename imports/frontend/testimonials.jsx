e47c9aa1dd9a7c6137fbc0b30b84b44f4e31e3c3
Redeemer40 <victorjonah199@gmail.com>
2020-05-04 18:54:36 +0100

    ...

--- imports/frontend/testimonials.jsx
+++ imports/frontend/testimonials.jsx
@@ -1,32 +1,38 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
+import Fade from "react-reveal/Fade";
 
 export class testimonials extends Component {
   render() {
     return (
-        <div id="testimonials">
-        <div className="container">
-          <div className="section-title text-center">
-            <h2>What our clients say</h2>
-          </div>
-          <div className="row">
-
-          {this.props.data ? this.props.data.map(d => 
-            <div className="col-md-4">
-            <div className="testimonial">
-              <div className="testimonial-image"> <img src={d.img} alt=""/> </div>
-              <div className="testimonial-content">
-                <p>"{d.text}"</p>
-                <div className="testimonial-meta"> - {d.name} </div>
-              </div>
+      <div id="testimonials">
+        <Fade bottom>
+          <div className="container">
+            <div className="section-title text-center">
+              <h2>What our clients say</h2>
+            </div>
+            <div className="row">
+              {this.props.data
+                ? this.props.data.map((d) => (
+                    <div className="col-md-4">
+                      <div className="testimonial">
+                        <div className="testimonial-image">
+                          {" "}
+                          <img src={d.img} alt="" />{" "}
+                        </div>
+                        <div className="testimonial-content">
+                          <p>"{d.text}"</p>
+                          <div className="testimonial-meta"> - {d.name} </div>
+                        </div>
+                      </div>
+                    </div>
+                  ))
+                : "loading"}
             </div>
           </div>
-          ) : 'loading'}
-          
-          </div>
-        </div>
+        </Fade>
       </div>
-    )
+    );
   }
 }
 
-export default testimonials
+export default testimonials;
