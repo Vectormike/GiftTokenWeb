7e58950356587714caf3938808c9667c89e63b17
Redeemer40 <victorjonah199@gmail.com>
2020-05-15 09:17:21 +0100

    privacy...

---
+++ imports/frontend/privacy.jsx
@@ -0,0 +1,265 @@
+import React, { Component } from "react";
+
+export default class privacy extends Component {
+  render() {
+    return (
+      <div>
+        This Privacy policy between TRUTHX EXCHNAGE (hereinafter referred to as
+        TRUTHX) and you, constitutes our commitment to your privacy on our
+        website, apps, social media platforms, administrative records, and
+        premises.
+        <br />
+        <h3>Your Privacy Rights </h3>
+        This Privacy Policy describes your privacy rights regarding our
+        collection, use, storage, sharing, and protection of your personal
+        information. It applies to the TRUTHX website, apps, platforms, and all
+        database applications, services, tools, and physical contact with us
+        regardless of how you access or use them. If you have created a
+        username, identification code, password or any other piece of
+        information as part of our access security measures, you must treat such
+        information as confidential, and you must not disclose it to any third
+        party. We reserve the right to disable any user identification code or
+        password, whether chosen by you or allocated by us, at any time, if in
+        our opinion you have failed to comply with any of the provisions of
+        these Conditions. If you know or suspect that anyone other than you know
+        your security details, you must promptly notify us at (INSERT TRUTHX
+        EMAIL HERE).
+        <br />
+        <h3>Consent</h3>
+        You accept this Privacy Policy when you give consent upon access to our
+        platforms, or use our services, content, features, technologies or
+        functions offered on our website, apps, digital platforms or visit any
+        of our offices for official or non-official purposes (collectively
+        “TRUTHX's services”). This Policy governs the use of TRUTHX's services
+        and intervention projects by our users and stakeholders unless otherwise
+        agreed through a written contract. We may amend this Privacy Policy at
+        any time by posting a revised version on our website, apps, platforms,
+        or placing such notice at conspicuous points at our office facilities.
+        The revised version will be effective 7-days after publication.
+        <br />
+        <h3>Your Personal Information</h3>
+        When you use TRUTHX's Services, we may collect information sent to us by
+        your computer, mobile phone, or other electronic access devices. The
+        automatically collected information includes but not limited to- data
+        about the pages you access, computer IP address, device ID or unique
+        identifier, device type, geo-location information, computer and
+        connection information, mobile network information, statistics on page
+        views, traffic to and from the sites, referral URL, ad data, standard
+        web log data, still and moving images. We may also collect the
+        information you provide us including but not limited to - information on
+        web forms, survey responses, account update information, email, phone
+        number, the organization you represent, official position,
+        correspondence with TRUTHX'S support services and telecommunication with
+        TRUTHX. We may also collect information about your transactions,
+        inquiries, and your activities on our platforms or service channels.  We
+        may also use the information provided by third parties like social media
+        sites. Information about you provided by other sites is not controlled
+        by TRUTHX and we are therefore not liable for how they use it.
+        <br />
+        <h3>What we do with your personal information</h3>
+        The purpose of collecting your personal information is to give you
+        efficient, enjoyable, and secure service. We may use your information
+        to: Provide TRUTHX's services and support; Process applications and send
+        notices about your transactions to requisite parties; Verify your
+        identity; Resolve disputes, collect fees, and troubleshoot problems;
+        Manage risk, detect, prevent, and/or remediate fraud or other
+        potentially prohibited or illegal activities; Detect, prevent or
+        remediate a violation of Laws, Regulations, Standards, Guidelines, and
+        Frameworks; Improve TRUTHX's Services by implementing aggregate customer
+        or user preferences; Measure the performance of the TRUTHX's Services
+        and improve the content, technology, and layout; Track information
+        breach and remediate such identified breaches; Manage and protect our
+        information technology and physical infrastructure; Contact you at any
+        time through your provided telephone number, email address, or other
+        contact details.
+        <br />
+        <h3>Cookies </h3>
+        Cookies are small files placed on your computer’s hard drive that
+        enables the website to identify your computer as you view different
+        pages. Cookies allow websites and applications to store your preferences
+        to present contents, options, or functions that are specific to you.
+        Like most interactive websites, our website uses cookies to enable the
+        tracking of your activity for the duration of a session. Our website
+        uses only encrypted session cookies which are erased either after a
+        predefined timeout period or once the user logs out of the platform and
+        closes the browser. Session cookies do not collect information from the
+        user’s computer. They will typically store information in the form of a
+        session identification that does not personally identify the user.
+        <br />
+        <h3>How we protect your personal information</h3>
+        We store and process your personal information on our computers in
+        Nigeria. Where we need to transfer your data to another country, such a
+        country must have an adequate data protection law. We will seek your
+        consent where we need to send your data to a country without an adequate
+        data protection law. We protect your information using physical,
+        technical, and administrative security measures to reduce the risks of
+        loss, misuse, unauthorized access, disclosure, and alteration. Some of
+        the safeguards we use are firewalls and data encryption, physical access
+        controls to our data centers, and information access authorization
+        controls.
+        <br />
+        <h3>How We Share your information within TRUTHX and other users</h3>
+        During your interaction with any of our platforms or premises, we may
+        provide Ministries, Departments, Agencies (MDAs), other organs of
+        government, private sector operators performing government functions,
+        with information such as your name, contact details, or other details
+        you provide us to perform their statutory mandate to you or third
+        parties. We work with third parties, banks, financial institutions, and
+        corporate bodies to perform TRUTHX's services and implement its mandate.
+        In doing so, a third party may share information about you with us, such
+        as your email address or mobile phone number. You accept that your
+        pictures and testimonials on all social media platforms about TRUTHX can
+        be used for limited promotional purposes by us. This does not include
+        your trademarked or copyrighted materials. From time to time we may send
+        you relevant information such as news items, enforcement notice,
+        statutorily mandated notices and other informative notices to help
+        TRUTHX serve you better. We may also share your personal information in
+        compliance with National or international laws; crime prevention and
+        risk management agencies and service providers.
+        <br />
+        <h3>Security</h3>
+        We promise to and will always hold your information securely. To prevent
+        unauthorized access to your information, we have implemented strong
+        controls and security safeguards at the technical and operational
+        levels. You should see the padlock symbol in your URL address bar once
+        you are successfully logged into the platform. The URL address will also
+        start with https:// depicting a secure webpage. SSL applies encryption
+        between two points such as your device and the connecting server. Any
+        data transmitted during the session will be encrypted before
+        transmission and decrypted at the receiving end. This is to ensure that
+        data cannot be read during transmission. TRUTHX has also taken measures
+        to comply with global Information Security Management Systems (ISMS) we,
+        therefore, have put in place digital and physical security measures to
+        limit or eliminate possibilities of data privacy breach incidents.
+        <br />
+        <h3>Data Confidentiality Rights</h3>
+        Your information is regarded as confidential and will not be divulged to
+        any third party except under legal and/or regulatory conditions. You
+        have the right to request sight of, and copies of any and all
+        information we keep on you. While TRUTHX is responsible for safeguarding
+        the information entrusted to us, your role in fulfilling confidentiality
+        duties includes, but is not limited to, adopting and enforcing
+        appropriate security measures such as non-sharing of passwords and other
+        platform login details, adherence with physical security protocols on
+        our platforms, dealing with only authorized officers of TRUTHX.
+        <br />
+        <h3>Links to Other Websites and Premises</h3>
+        Certain transaction processing channels may require links to other
+        websites other than ours. Please note that TRUTHX is not responsible and
+        has no control over websites outside its domain. We do not monitor or
+        review the content of other party’s websites which are linked from our
+        website or media platforms. Opinions expressed or materials appearing on
+        such websites are not necessarily shared or endorsed by us, and TRUTHX
+        should not be regarded as the publisher of such opinions or materials.
+        Please be aware that we are not responsible for the privacy practices,
+        or content of these sites. We encourage our users to be aware of when
+        they leave our site and to read the privacy statements of these sites.
+        You should evaluate the security and trustworthiness of any other site
+        connected to this site or accessed through this site yourself, before
+        disclosing any personal information to them. TRUTHX will not accept any
+        responsibility for any loss or damage in whatever manner, howsoever
+        caused, resulting from your disclosure to third parties of personal
+        information.
+        <br />
+        <h3>Governing Law</h3>
+        This Privacy Policy is made according to and in compliance with the
+        Nigeria Data Protection Regulation (2019) and other relevant Nigerian
+        laws, regulations, or international conventions applicable to Nigeria.
+        Where any provision of this Policy is deemed inconsistent with a law,
+        regulation, or convention, such provision shall be subject to the
+        overriding law, regulation, or convention.
+        <br />
+        <h3>COOKIES POLICY</h3>
+        Cookies are small text files that are transferred from our websites,
+        apps, platforms, or portals and stored on your device. We use cookies to
+        help us provide you with personalized service and to help make our
+        websites or portals better for you. Our cookies may be session cookies
+        (temporary cookies that identify and track users within our websites or
+        portals which are deleted when you close your browser or leave your
+        session) or persistent cookies (cookies which enable our websites or
+        portals to “remember” who you are and to remember your preferences
+        within our websites or portals and which will stay on your computer or
+        device after you close your browser or leave your session We use the
+        following different types of cookies:
+        <h4>Essential cookies </h4>
+        These are cookies which are needed for our websites or portals to
+        function properly, for example, these cookies allow you to access secure
+        areas of our website/portal.
+        <h4>Performance cookies</h4>
+        These cookies collect information about how visitors and users use our
+        websites and portals. These cookies don’t collect information that
+        identifies a visitor or user. All information these cookies collect is
+        aggregated and therefore anonymous. We only use these cookies to improve
+        how our website and portal.
+        <h4>Functionality cookies</h4>
+        These cookies allow our websites and portals to remember choices you
+        make (such as your user name, language, or the region you are in) and
+        provide enhanced, more personal features. These cookies can also be used
+        to remember changes you have made to text size, fonts, and other parts
+        of web pages that you can customize. They may also be used to provide
+        services you have asked for. The information these cookies collect may
+        be anonymized and they cannot track your browsing activity on other
+        websites. Like most interactive websites, our website uses cookies to
+        enable the tracking of your activity for the duration of a session. Our
+        website uses only encrypted session cookies which are erased either
+        after a predefined timeout period or once the user logs out of the
+        platform and closes the browser. Session cookies do not collect
+        information from the user’s computer. They will typically store
+        information in the form of a session identification that does not
+        personally identify the user.
+        <br />
+        <h3>DATA PROTECTION POLICY </h3>
+        TRUTHX's Data Protection Policy refers to our commitment to treat the
+        information of employees, customers, stakeholders, and other interested
+        parties with the utmost care and confidentiality. With this policy, we
+        ensure that we gather, store, and handle data fairly, transparently, and
+        with respect towards individual rights.
+        <br />
+        <h3>Scope</h3>
+        This policy refers to all parties who provide any amount of information
+        to us.
+        <br />
+        <h3>What is covered under the Data Protection Policy? </h3>
+        TRUTHX's employees must follow this policy. Freelancers, consultants,
+        partners, and any other external entity are also covered. Generally, our
+        policy refers to anyone we collaborate with or acts on our behalf and
+        may need occasional access to data.
+        <br />
+        <h3>Policy Elements</h3>
+        As part of our operations, we need to obtain and process information.
+        This information includes any offline or online data that makes a person
+        identifiable such as names, addresses, usernames and passwords, digital
+        footprints, photographs, National identity numbers, financial data, etc.
+        TRUTHX transparently collects this information and only with the full
+        cooperation and knowledge of interested parties. Once this information
+        is available to us, the following rules apply.
+        <br />
+        <h4>Our data will be:</h4>
+        Accurate and kept up-to-date; Collected fairly and for lawful purposes
+        only; Processed by the company within its legal and moral boundaries;
+        Protected against any unauthorized or illegal access by internal or
+        external parties. Our data will not be: Transferred to organizations,
+        states or countries that do not have adequate data protection policies;
+        Distributed to any party other than the ones agreed upon by the data’s
+        owner (exempting legitimate requests from law enforcement authorities).
+        In addition to ways of handling data, TRUTHX has direct obligations
+        towards people to whom the data belongs. Specifically we: Let people
+        know which of their data is collected; Inform people about how we’ll
+        process their data; Inform people about who has access to their
+        information; Have provisions in cases of lost, corrupted or compromised
+        data; Allow people to request that we modify, erase, reduce or correct
+        data contained in our databases.
+        <br />
+        <h3>Actions</h3>
+        To exercise data protection we’re committed to: Restricting and
+        monitoring access to sensitive data; Developing transparent data
+        collection procedures; Training employees in online privacy and security
+        measures; Building secure networks to protect online data from
+        cyberattacks; Establishing clear procedures for reporting privacy
+        breaches or data misuse; Including contract clauses or communicate
+        statements on how we handle data; Establishing data protection
+        practices.
+      </div>
+    );
+  }
+}
