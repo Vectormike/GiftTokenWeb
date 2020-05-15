578dff65a5293839cc25fd6955fe25c5206258f6
Redeemer40 <victorjonah199@gmail.com>
2020-05-15 09:17:08 +0100

    Added Privacy route.

--- imports/routes/router.jsx
+++ imports/routes/router.jsx
@@ -1,15 +1,15 @@
-import React, { Component } from 'react'
-
-import { BrowserRouter as Router, Route, Link } from "react-router-dom";
-import App from '../frontend/App';
-import Backend from '../backend/App';  // Could be used to implement a backend down the line
+import React, { Component } from "react";
+import Privacy from "../frontend/privacy";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+import App from "../frontend/App";
+import Backend from "../backend/App"; // Could be used to implement a backend down the line
 const AppRouter = () => (
-    <Router>
-  <div>
-        <Route path="/"  exact component={App} />
-        </div>
-    </Router>
-  );
-  
-  export default AppRouter;
-  
+  <Router>
+    <div>
+      <Route path="/" exact component={App} />
+      <Route path="/privacy" exact component={Privacy} />
+    </div>
+  </Router>
+);
+
+export default AppRouter;
